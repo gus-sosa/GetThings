c4d1d828aa12258cb70cae066cfd8b66ec951107
Gustavo Sosa <g.sosacuervo1@gmail.com>
2016-11-02 09:58:52 -0500

    PluralsightDownloader through python removed and implemented using node.js through the terminal

--- GetThings.Downloader/PluralsightDownloader.cs
+++ GetThings.Downloader/PluralsightDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using GetThins.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,61 @@ namespace GetThings.Downloader
     {
         public bool CanDownloadResource(string resource)
         {
-            return resource.Contains("pluralsight.com");
+            return resource.Contains("pluralsight.com") && ThridPartyProgramInstalled();
+        }
+
+        private bool ThridPartyProgramInstalled()
+        {
+            return true;//TODO: Check that node-pd (a program from node.js) is installed in the pc
         }
 
         public bool Download(string resource, BaseInfo info)
         {
-            throw new NotImplementedException();
+            string idResource = GetId(resource);
+
+            ConfigEnvironment(info);
+
+            var process = new Process()
+            {
+                StartInfo=new ProcessStartInfo()
+                {
+                    Arguments=$"/C pd download {idResource}",
+                    FileName="cmd.exe"
+                }
+            };
+
+            try
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ConfigEnvironment(BaseInfo info)
+        {
+            var procces = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    Arguments = $"/C pd config -u {info.Username} -p {info.Password} -s {info.PathDirectory}",
+                    FileName="cmd.exe"
+                }
+            };
+
+            procces.Start();
+            procces.WaitForExit();
+        }
+
+        private string GetId(string resource)
+        {
+            var segments = resource.Split('/');
+            return segments[segments.Length - 1];
         }
     }
 }
