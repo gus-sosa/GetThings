4a778dcb60e0dae80fb810ca63b6b9ad2470b602
Gustavo Sosa <g.sosacuervo1@gmail.com>
2016-11-02 01:54:45 -0500

    CanDownloadResource implemented and PluralsightDownloader created

---
+++ GetThings.Downloader/PluralsightDownloader.cs
@@ -0,0 +1,22 @@
+﻿using GetThins.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetThings.Downloader
+{
+    public class PluralsightDownloader : IDownloader
+    {
+        public bool CanDownloadResource(string resource)
+        {
+            return resource.Contains("pluralsight.com");
+        }
+
+        public bool Download(string resource, BaseInfo info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
